a1e2d6ca2a2f9987d4feb1ee59924a0ef92f0d04
Nico Popy <nicolas.popy@gmail.com>
2021-05-11 23:07:57 +0200

    Mise en page produit suite et selection/option

--- src/produit.js
+++ src/produit.js
@@ -24,26 +24,37 @@ fetch("http://localhost:3000/api/cameras/" + id)
 
 function afficheProduit(prod){ 
     try{
-        //Affiche titre
-        var titre = document.getElementById("titre");
-        var h1 = titre.getElementsByTagName("h1")[0];
-        h1.innerHTML = prod.name;
+      //Affiche titre
+      var titre = document.getElementById("titre");
+      var h1 = titre.getElementsByTagName("h1")[0];
+      h1.innerHTML = prod.name;
 
-        //Affiche prix
-        var prix = document.getElementById("prix");
-        var strong = prix.getElementsByTagName("strong")[0];
-        strong.innerHTML = prod.price;
+      //Affiche prix
+      var prix = document.getElementById("prix");
+      var strong = prix.getElementsByTagName("strong")[0];
+      strong.innerHTML = prod.price + " €";
 
-        //Affiche image
-        var photo = document.getElementById("photo");
-        var img = photo.getElementsByTagName("img")[0];
-        img.setAttribute("src",prod.imageUrl); 
+      //Affiche image
+      var photo = document.getElementById("photo");
+      var img = photo.getElementsByTagName("img")[0];
+      img.setAttribute("src", prod.imageUrl);
 
-        //Affiche description
-        var description = document.getElementById("description");
-        var p = description.getElementsByTagName("p")[0];
-        p.innerHTML = prod.description;
+      //Affiche description
+      var description = document.getElementById("description");
+      var p = description.getElementsByTagName("p")[0];
+      p.innerHTML = prod.description;
 
+      //Affiche Option
+      var option = document.getElementsByClassName("form-select")[0];
+
+      console.log(prod);
+      for (let opt in prod.lenses) {
+         var optionselect = document.createElement("option");
+         optionselect.value = prod.lenses[opt];
+         optionselect.innerHTML = prod.lenses[opt];
+         option.appendChild(optionselect);
+        console.log(prod.lenses[opt]);
+      }
     }catch(ex)
     {
         console.error(ex);
